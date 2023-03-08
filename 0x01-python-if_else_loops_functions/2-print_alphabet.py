d65361063a4be937203daebccb7456954d7457fa
TgGeda <124788842+TgGeda@users.noreply.github.com>
2023-03-08 19:37:18 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print("{:s}".format(chr(i)), end="")
+
+for i in range(97, 123):
+    if i == 113 or i == 101:
+        continue
+    print("{:c}".format(i), end="")
