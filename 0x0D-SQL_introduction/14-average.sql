e675b09c9ba2fe0a48ca2c13505132b87c04992e
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-16 13:05:51 +0300

    a script that computes the score average of all records in the table second_table of the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- A script that computes the score average of all records in the table second_table of the database hbtn_0c_0
+-- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT AVG(score) AS average FROM hbtn_0c_0.second_table;
