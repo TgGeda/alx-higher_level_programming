8941e65c1a8d496693bd78b712f942eda3604e2d
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-16 12:09:30 +0300

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
---a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL serve
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Show the create table statement
+-- Execute: cat 5-full_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SHOW CREATE TABLE first_table;
