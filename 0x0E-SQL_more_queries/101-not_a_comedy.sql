b019a6ab606f23948636928cf1e8f43a270b5809
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-17 14:52:06 +0300

    No Comedy tonight'S problem bug fix

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,13 @@
 -- A script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT title FROM tv_shows 
-WHERE id NOT IN 
-(SELECT tv_show_id FROM tv_show_genres 
-WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')) 
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- uses a database to list all rows not linked to one row
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
 ORDER BY title ASC;
