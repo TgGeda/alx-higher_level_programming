8eadfadf4cedb8dc633ce7a1bd530304a6fb325b
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-17 14:47:59 +0300

    Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,3 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- lists all rows of a database that have one column in common
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN
+(SELECT genre_id FROM tv_show_genres WHERE show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter'))
+ORDER BY tv_genres.name ASC;
