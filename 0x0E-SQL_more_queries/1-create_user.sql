1b74c162d846981fb6501fe1af5450cb0efaf10b
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-17 12:09:51 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,7 @@
    -- password set to user_0d_1_pwd
    -- If the user user_0d_1 already exists, script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
 FLUSH PRIVILEGES;
+
